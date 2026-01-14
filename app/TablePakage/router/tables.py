a1ec527ae5a01f371ddf75240b1cd8f3bd6ce677
flumine7 <flumine@yandex.ru>
2026-01-14 17:29:12 +0300

    CORE-01 added new endpoint for full upload from xslx-file. Made new file router_utils.py for function, which used in endpoints.

--- app/TablePakage/router/tables.py
+++ app/TablePakage/router/tables.py
@@ -19,7 +19,109 @@ router = APIRouter(prefix="/tables", tags=["Tables"])
 
 # === Table Schema Endpoints ===
 
-@router.post("/upload_xlsx", description="Импорт параметров из XLSX.")
+@router.post("/upload_full_xlsx", description="Импорт всех параметров из XLSX.")
+async def import_excel(
+        product_name: str,
+        file: UploadFile = File(...),
+        db: AsyncSession = Depends(get_db)
+):
+    table_name = f"{to_sql_name_lat(product_name)}_table"
+
+    product_result = await db.execute(
+        text("SELECT id FROM products WHERE name = :name"),
+        {"name": product_name}
+    )
+    product_id = product_result.scalar_one_or_none()
+
+    if product_id is None:
+        return {"message": f"Продукция '{product_name}' не найдена."}
+
+    # 1. Читаем Excel
+    df = pd.read_excel(file.file)
+    df = df.where(pd.notnull(df), None)
+
+    # 2. Получаем колонки БД (без id)
+    result = await db.execute(
+        text("""
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = :table_name
+              AND column_name != 'id'
+        """),
+        {"table_name": table_name}
+    )
+
+    db_columns = {row[0] for row in result.fetchall()}
+
+    # 3. Сопоставление: транслит → оригинальное имя из Excel
+    excel_map = {
+        to_sql_name_lat(col): col
+        for col in df.columns
+        if col.lower() != "id"
+    }
+
+    common_columns =set(excel_map.keys())
+
+    if not common_columns:
+        return {"message": "Нет колонок для вставки"}
+
+    missing = common_columns - db_columns
+
+    # создаём недостающие
+    for col in missing:
+        await db.execute(
+            text(f'ALTER TABLE {table_name} ADD COLUMN "{col}" TEXT')
+        )
+        await db.execute(
+            text("""
+                            INSERT INTO parameter_schemas (name, type, table_name, product_id)
+                            SELECT
+                            CAST(:name AS VARCHAR),
+                            'Table',
+                            CAST(:table_name AS VARCHAR),
+                            :product_id
+                            WHERE NOT EXISTS (
+                                SELECT 1
+                                FROM parameter_schemas
+                                WHERE name = :name
+                                  AND product_id = :product_id
+                            )
+                        """),
+            {
+                "name": col,
+                "table_name": product_name,
+                "product_id": product_id
+            }
+        )
+
+    await db.commit()
+
+    # 4. Формируем INSERT
+    columns_sql = ", ".join(common_columns)
+    values_sql = ", ".join(f":{col}" for col in common_columns)
+
+    insert_sql = text(f"""
+        INSERT INTO {table_name} ({columns_sql})
+        VALUES ({values_sql})
+    """)
+
+    # 5. Вставляем строки
+    for _, row in df.iterrows():
+        values = {
+            col: str(row[excel_map[col]]) if row[excel_map[col]] is not None else None
+            for col in common_columns
+        }
+        await db.execute(insert_sql, values)
+
+    await db.commit()
+
+    return {
+        "table": table_name,
+        "inserted_rows": len(df),
+        "used_columns": list(common_columns)
+    }
+
+@router.post("/upload_matched_params_xlsx", description="Импорт параметров из XLSX, которые уже есть в базе данных.")
 async def import_excel(
         product_name: str,
         file: UploadFile = File(...),
@@ -45,7 +147,8 @@ async def import_excel(
 
     # 3. Сопоставление: транслит → оригинальное имя из Excel
     excel_map = {
-        to_sql_name_lat(col): col for col in df.columns
+        to_sql_name_lat(col): col
+        for col in df.columns
     }
 
     # 4. Пересечение
@@ -121,7 +224,7 @@ async def download_xlsx(
         for col in df.columns
     ]
     df = df.applymap(
-        lambda x: to_sql_name_kir(x) if isinstance(x, str) else x
+        lambda x: x if isinstance(x, str) else x
     )
 
     # 5. Создаём временный XLSX
--- app/TablePakage/utils/router_utils.py
+++ app/TablePakage/utils/router_utils.py
@@ -1,11 +1,19 @@
+import re
 from transliterate import translit
 
 
 def to_sql_name_lat(name: str) -> str:
     # Замена кириллицы на латиницу транслитом
-    return translit(name.lower(), 'ru', reversed=True).replace(" ", "_")
+    name = name.lower()
+    name = translit(name, "ru", reversed=True)
+    name = re.sub(r"[^a-z0-9_]", "_", name)
+    name = re.sub(r"_+", "_", name)
+    return name.strip("_")
 
 
 def to_sql_name_kir(name: str) -> str:
     # Замена латиницы на кириллицу транслитом
-    return translit(name.lower(), 'ru').replace("_", " ")
+    name = name.lower()
+    name = translit(name, "ru")
+    name = re.sub(r"_", " ", name)
+    return name.strip("_")
