b38348185ab5d3d1d1efe617f3f799b5d925bf43
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-06 12:58:42 +0400

    КОДОВЫЙ ФОРМУЛЬНЫЙ ПАРАМЕТР

--- app/TableSearch/router/module_search.py
+++ app/TableSearch/router/module_search.py
@@ -199,7 +199,7 @@ async def process_table_data(
         # print(new_params, table_name)
         parameters = await search_formula(db, new_params, table_name)
 
-        parameters = sorted(parameters, key=lambda param: param['id'])
+        # parameters = sorted(parameters, key=lambda param: param['id'])
 
         return {
             "product_id": product_id,
