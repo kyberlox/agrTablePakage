e0f5e038bcfbbee57824d70e00f816f96888c29e
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-06 15:38:16 +0400

    КОДОВЫЙ ФОРМУЛЬНЫЙ ПАРАМЕТР

--- app/FormulaPakage/utils/code_mode.py
+++ app/FormulaPakage/utils/code_mode.py
@@ -60,7 +60,7 @@ class CodeParametr:
                 },
                 {
                     'id': note_params_info["id"],
-                    'name': note_params_info["id"],
+                    'name': note_params_info["name"],
                     'description': note_params_info["description"],
                     'visibility': True,
                     'required_type':  "list",
