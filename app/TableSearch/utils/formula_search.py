cca4a23d717b337964bd432cbe17aa919945ebec
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-06 12:10:20 +0400

    КОДОВЫЙ ФОРМУЛЬНЫЙ ПАРАМЕТР

--- app/TableSearch/utils/formula_search.py
+++ app/TableSearch/utils/formula_search.py
@@ -78,9 +78,9 @@ async def code_params(note_params_info, db, user_params):
     #тут надо вызвать нужную функцию по её названию из БД
     cp_class = CodeParametr()
     cp_method_name = note_params_info["function_name"]
-    cp_method = getattr(cp_class, cp_method_name, note_params_info)
+    cp_method = getattr(cp_class, cp_method_name)
 
-    return cp_method(user_params)
+    return cp_method(user_params, , note_params_info)
 
 
 async def condition_params(param_info, db, params):
