b5dcba4c6e4ce9ccd7ce9112bcf983106dd68ca8
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-08 15:49:28 +0400

    расчет парметров смеси сред

--- app/FormulaPakage/utils/code_mode.py
+++ app/FormulaPakage/utils/code_mode.py
@@ -337,7 +337,7 @@ class CodeParametr:
 
             #чтобы проще было заполнять
             all_columns_names = await db.execute(text("SELECT column_name FROM information_schema.columns WHERE table_name = \'{searching_table_name}\';"))
-            print("Список колонок таблицы: ", all_columns_names.scalar_one_or_none())
+            print("Список колонок таблицы: ", all_columns_names)
             env_keys = {
                 "name" : "nazvanie_rabochej_sredy",
                 "environment" : "",
