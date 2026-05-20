10c39d891635963088050c457eb8b77f7979214c
kyberlox <64195832+kyberlox@users.noreply.github.com>
2026-05-20 12:13:18 +0400

    пытаюсь всё спасти

--- app/TableSearch/router/module_search.py
+++ app/TableSearch/router/module_search.py
@@ -254,7 +254,6 @@ async def process_table_data(
     }
 
     # Собираем значения параметров ! ???
-    print("ngfhgfhg", row, row[col], col)
     parameters = {
         param_name: sorted(str(v) for v in row[col])
         for col, param_name in column_to_param.items()
